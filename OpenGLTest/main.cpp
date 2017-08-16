6b6c8beef7ae671177cdd414ee172ff0d0ce5009
Shubhankar Mohapatra <mshubhankar@yahoo.co.in>
2017-08-16 17:44:53 +0530

    changes

---
+++ OpenGLTest/main.cpp
@@ -0,0 +1,9 @@
+//
+//  main.cpp
+//  OpenGLTest
+//
+//  Created by Shubhankar Mohapatra on 16/08/17.
+//  Copyright © 2017 Shubhankar Mohapatra. All rights reserved.
+//
+
+#include "main.hpp"
---
+++ OpenGLTest/main.hpp
@@ -0,0 +1,14 @@
+//
+//  main.hpp
+//  OpenGLTest
+//
+//  Created by Shubhankar Mohapatra on 16/08/17.
+//  Copyright © 2017 Shubhankar Mohapatra. All rights reserved.
+//
+
+#ifndef main_hpp
+#define main_hpp
+
+#include <stdio.h>
+
+#endif /* main_hpp */
